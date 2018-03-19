dc4dd699dfceb04e438c40270e1b5301e72b53c4
Yanova Tatyana <yanovatatyana1@gmail.com>
2018-03-19 17:12:35 +0200

    add test

---
+++ tests/test/HelloTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use VM\Test\TestCase\Selenium;
+
+class HelloTest extends Selenium {
+	public function setUp()
+    {
+        parent::setUp();        
+        $this->webDriver->get($this->startUrl);        
+    }
+    public function testHello() {
+		 $linkText = 'hello';
+		 $failMessage = "Text  not found " ;         
+         $this->testCase->assertTrue($this->isElementPresent(\WebDriverBy::xpath(sprintf("(//*[text()='%s'])",$linkText))), $failMessage);       
+        
+    }
+    
+    protected function isElementPresent(\WebDriverBy $by)
+    {        
+        try {
+            $element = $this->webDriver->findElement($by);  
+            if($element->isDisplayed()){                   
+                return true;
+            }            
+            return false;
+        }
+        catch (\Throwable  $e) {              
+            return false;
+        }
+    }   
+	
+	}
