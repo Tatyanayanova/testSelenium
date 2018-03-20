ec2c4cde4958967025a475cbf16c28289468842d
Yanova Tatyana <yanovatatyana1@gmail.com>
2018-03-20 17:38:31 +0200

    test2

---
+++ tests/test/Hello2Test.php
@@ -0,0 +1,45 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class Hello2Test extends TestCase {
+
+    protected $webDriver;
+    protected $startUrl;
+
+    public function setUp() {
+        $options = new \ChromeOptions();
+        $options->addArguments(array(
+            '--window-size=1324,768',
+        ));
+        $caps = \DesiredCapabilities::chrome();
+        $caps->setCapability(\ChromeOptions::CAPABILITY, $options);
+        $this->webDriver = \RemoteWebDriver::create('http://localhost:4444/wd/hub', $caps);
+        $this->webDriver->manage()->timeouts()->implicitlyWait(10);
+        $this->startUrl = "http://testsel.dz/manager/";
+        $this->webDriver->get($this->startUrl);
+    }
+
+    public function testHello() {
+        $linkText = 'Hello';
+        $failMessage = "Text  not found ";        
+        $this->assertTrue($this->isElementPresent(\WebDriverBy::xpath(sprintf("(//*[text()='%s'])", $linkText))), $failMessage);
+    }
+
+    protected function isElementPresent(\WebDriverBy $by) {
+        try {
+            $element = $this->webDriver->findElement($by);
+            if ($element->isDisplayed()) {                
+                return true;
+            }
+            return false;
+        } catch (\Throwable $e) {            
+            return false;
+        }
+    }
+
+    public function tearDown() {
+        $this->webDriver->quit();
+    }
+
+}
