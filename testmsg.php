093f6b2c48b95a91dded46db3d7861d59290d884
Yanova Tatyana <yanovatatyana1@gmail.com>
2018-04-26 11:06:25 +0300

    slack

--- testmsg.php
+++ testmsg.php
@@ -6,6 +6,173 @@
  * and open the template in the editor.
  */
 
+
+define('HTML_STATISTIC_URL', 'https://login.vetmanager.ru/domenStatistic/allStatistic');
+define('API_KEY', '9205814c8bc98857f5e07f3bae30ad68');
+define('CHANNEL', '#test');
+
+function slackJeson($data) {
+    $ch = curl_init("https://tanyayanova.slack.com/api/chat.postMessage");
+    $data_string = json_encode($data);    
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Authorization: Bearer xoxp-335261537463-334177305284-352739427747-8266018d0d29ac3644f01fe8d851fe9d',
+        'Content-Length: ' . strlen($data_string))
+    );
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
+
+function scheduleSlackJeson() {
+
+    $report = json_decode(file_get_contents(HTML_STATISTIC_URL . '?key=' . API_KEY . '&json=true'));
+    $msgEndTariffPlan = array();
+    $msgEndTariffPlan[] = [
+                        "title" => "Доменное имя",
+                        "short" => true
+                    ];
+            $msgEndTariffPlan[] =  [
+                        "title" => "Окончание тарифа",
+                        "short" => true
+                    ];
+    if (!empty($report->endTariffPlan)) {
+        foreach ($report->endTariffPlan as $client) {            
+            $msgEndTariffPlan[] = [
+                        "value" => $client->domain_name,
+                        "short" => true
+                    ];
+            $msgEndTariffPlan[] =  [
+                        "value" => $client->tariff_valid_until,
+                        "short" => true
+                    ];
+        }
+    } 
+    $msgChiters = '';
+    if (!empty($report->chitters)) {
+        foreach ($report->chitters as $cheter) {            
+            
+            $msgChiters = $msgChiters . $cheter->domain_name . "(" . $cheter->cnt . "),";
+        }
+    }
+    
+    $data = [
+        "channel" => CHANNEL,        
+        "username" => "SlackBot",
+        "attachments" => [
+            ["text" => 'ПОКАЗАТЕЛИ'],
+            ["color" => "#2eb886"],
+            ["fields" => [
+                    [
+                        "title" => "Наименование",
+                        "short" => true
+                    ],
+                    [
+                        "title" => "Значение (в скобочках показатель за предыдущий месяц)",
+                        "short" => true
+                    ],
+                    [
+                        "value" => "Всего пользователей",
+                        "short" => true
+                    ],
+                    [
+                        "value" => $report->commonData->userCount,
+                        "short" => true
+                    ],
+                    [
+                        "value" => "Оплаченных клиник",
+                        "short" => true
+                    ],
+                    [
+                        "value" => $report->commonData->paidActiveUser,
+                        "short" => true
+                    ],
+                    [
+                        "value" => "Пользователей в оплаченных клиниках",
+                        "short" => true
+                    ],
+                    [
+                        "value" => $report->commonData->tariffsUserInfo->current->sum_users . " (" . $report->commonData->tariffsUserInfo->previus->sum_users . ')',
+                        "short" => true
+                    ],
+                    [
+                        "value" => "Пользователей в среднем на клинику",
+                        "short" => true
+                    ],
+                    [
+                        "value" => $report->commonData->tariffsUserInfo->current->avg_users . " (" . $report->commonData->tariffsUserInfo->previus->avg_users . ')' ,
+                        "short" => true
+                    ],
+                    [
+                        "value" => "Сумма приблизительно в месяц (300 рублей на пользователя)",
+                        "short" => true
+                    ],
+                    [
+                        "value" => $report->commonData->tariffsUserInfo->current->app_sum . ' (' . $report->commonData->tariffsUserInfo->previus->app_sum . ')' ,
+                        "short" => true
+                    ],
+                    [
+                        "value" => "Бесплатные пользователи",
+                        "short" => true
+                    ],
+                    [
+                        "value" => $report->commonData->freeUserCount ,
+                        "short" => true
+                    ],
+                    [
+                        "value" => "Активные пользователи сегодня (вчера)",
+                        "short" => true
+                    ],
+                    [
+                        "value" => $report->commonData->activeUsersToday->current . " (" . $report->commonData->activeUsersToday->last . ')' ,
+                        "short" => true
+                    ],
+                    [
+                        "value" => "Регистраций ",
+                        "short" => true
+                    ],
+                    [
+                        "value" => $report->commonData->registrationCount->current . ' (' . $report->commonData->registrationCount->last . ')' ,
+                        "short" => true
+                    ],
+                    [
+                        "value" => "Количество новых оплативших",
+                        "short" => true
+                    ],
+                    [
+                        "value" => $report->commonData->newPaidUser->current . " (" . $report->commonData->newPaidUser->last . ')',
+                        "short" => true
+                    ],
+                    [
+                        "value" => "Просроченные клиенты",
+                        "short" => true
+                    ],
+                    [
+                        "value" => $report->commonData->paidNotActiveUser,
+                        "short" => true
+                    ]
+                ]
+            ],
+            ["text" => 'КОЛИЧЕСТВО ВЕРСИЙ ДЛЯ ОДНОГО ДОМЕНА ЗА СУТКИ'],
+            ["fields" =>  $msgEndTariffPlan ],
+            ["text" => 'КОЛИЧЕСТВО ВЕРСИЙ ДЛЯ ОДНОГО ДОМЕНА ЗА СУТКИ'],
+            ["text" => $msgChiters],
+        ]
+    ];
+
+
+    slackJeson($data);
+}
+slack('hello', '#test');
+var_dump(scheduleSlackJeson());
+
+
+
 function slack($message, $channel)
 {
     $ch = curl_init("https://tanyayanova.slack.com/api/chat.postMessage");
@@ -25,63 +192,44 @@ function slack($message, $channel)
     return $result;
 }
 
-slack('hello', '#test');
-
-///////////////////////////
-
-/*class DomenStatisticController extends Controller
-public function actionAllStatistic($key, $json = false) {
-        if ($key == Yii::app()->params['VETMANAGER_API_KEY']) {
-            $model = new DomainsLog;
-            if($json == true){
-                $this->renderPartial('allstatisticsjson', array('model' => $model));
-            }else{
-                $this->renderPartial('allstatistics', array('model' => $model));
-            }
-        }
-    }
-
-  ///////////////////////////////////////////  
-    
-    
-   public function runScheduleSlack()
+function scheduleSlack()
     {
-        $time = date('H:i') . ':00';
-        //$communication = new Communication();
-        if ($time >= '07:00:00') {
-            $report = '<p><b>Показатели:</b></p> <br />' . file_get_contents(self::HTML_STATISTIC_URL . '?key=' . self::API_KEY);
-            var_dump('self::HTML_STATISTIC_URL', self::HTML_STATISTIC_URL);
-            var_dump('self::API_KEY', self::API_KEY);
-            //var_dump($report);
-            //$communication->sendNotificationToHipChat($report);
-        }
-         
-         $this->slack("This is a line of text.\nAnd this is another one.", '#test');
-         echo 'ok';
-                 
+        $time = date('H:i') . ':00';        
+        if ($time == '07:00:00') {
+            $report =  json_decode(file_get_contents(HTML_STATISTIC_URL . '?key=' . API_KEY.'&json=true'));
+            $msgEndTariffPlan = '';              
+            if (!empty($report->endTariffPlan)){             
+                foreach ($report->endTariffPlan as $client){
+                    $msgEndTariffPlan = $msgEndTariffPlan.str_pad($client->domain_name,130).str_pad($client->tariff_valid_until,130)."\n";  
+                }
+            }   
+            $msgChiters = '';    
+            if (!empty($report->chitters)){             
+                foreach ($report->chitters as $cheter){
+                    $msgChiters = $msgChiters.$cheter->domain_name."(". $cheter->cnt. "),";
+                }
+            }            
+            $message = ">                                                    ПОКАЗАТЕЛИ\n"
+                   . " *Наименование*                                                                                     *Значение* (в скобочках показатель за предыдущий месяц)\n"                
+                    .  str_pad("Всего пользователей ",130).$report->commonData->userCount."\n"
+                    .  str_pad("Оплаченных клиник",126).$report->commonData->paidActiveUser."\n"
+                    .  str_pad("Пользователей в оплаченных клиниках",126).$report->commonData->tariffsUserInfo->current->sum_users . " (". $report->commonData->tariffsUserInfo->previus->sum_users. ')'."\n"
+                    .  str_pad("Пользователей в среднем на клинику",127).$report->commonData->tariffsUserInfo->current->avg_users. " (". $report->commonData->tariffsUserInfo->previus->avg_users . ')'."\n"
+                    .  str_pad("Сумма приблизительно в месяц (300 рублей на пользователя)",120).$report->commonData->tariffsUserInfo->current->app_sum. ' ('. $report->commonData->tariffsUserInfo->previus->app_sum. ')'."\n"
+                    .  str_pad("Бесплатные пользователи",128).$report->commonData->freeUserCount."\n"
+                    .  str_pad("Активные пользователи сегодня(вчера)",127).$report->commonData->activeUsersToday->current. " (".$report->commonData->activeUsersToday->last. ')'."\n"
+                    .  str_pad("Регистраций ",130).$report->commonData->registrationCount->current. ' ('. $report->commonData->registrationCount->last. ')'."\n"
+                    .  str_pad("Количество новых оплативших",126).$report->commonData->newPaidUser->current. " (". $report->commonData->newPaidUser->last. ')'."\n"
+                    .  str_pad("Просроченные клиенты",126).$report->commonData->paidNotActiveUser."\n" 
+                    . ">                                                ОКОНЧАНИЕ ТАРИФНОГO ПЛАНА\n"
+                    .str_pad("*Доменное имя*",130).str_pad("*Окончание тарифа*",130)."\n"               
+                    .$msgEndTariffPlan
+                    .">                                          КОЛИЧЕСТВО ВЕРСИЙ ДЛЯ ОДНОГО ДОМЕНА ЗА СУТКИ\n"
+                    .$msgChiters."\n"
+                    ;     
+             
+            slack($message, '#statistics');
         
-        
+        }  
     }
 
-    protected  function slack($message, $channel)
-    {
-        $ch = curl_init("https://tanyayanova.slack.com/api/chat.postMessage");
-        $data = http_build_query([
-            "token" => "xoxp-335261537463-334177305284-347122349636-2bbb07669f5c03c5f2a3469a6e4fa9b7",
-            "channel" => $channel, //"#mychannel",
-            "text" => $message, //"Hello, Foo-Bar channel message.",
-            "username" => "MySlackBot",
-        ]);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        $result = curl_exec($ch);
-        curl_close($ch);
-
-        return $result;
-    }
-}
-
-$test = new ScheduleCommand();
-$test->runScheduleSlack();    
